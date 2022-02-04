9eac8d5c6a0efa39d58c9599bb8336bf26a8f337
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2022-02-04 11:51:18 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Glitch Art Bot
+A telegram normal image to glitch art image converter bot
+
+---
+
+## Variables
+
+- `API_HASH` Your API Hash from my.telegram.org
+- `API_ID` Your API ID from my.telegram.org
+- `BOT_TOKEN` Your bot token from @BotFather
+
+---
+
+## Commands
+
+```
+start - For start bot
+```
+
+---
---
+++ main.py
@@ -0,0 +1,74 @@
+
+import os
+import glitchart
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+Bot = Client(
+    "Glitch-Art-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+START_TEXT = """Hello {},
+I am a photo to glitch art telegram bot.
+
+Made by @FayasNoushad"""
+START_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton('Group', url='https://telegram.me/musicwithalby'),
+            InlineKeyboardButton('Feedback', url='https://telegram.me/i_am_albin_praveen')
+        ]
+    ]
+)
+PATH = os.environ.get("PATH", "./DOWNLOADS")
+
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        reply_markup=START_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Bot.on_message(filters.private & filters.photo)
+async def glitch_art(bot, update):
+    download_path = PATH + "/" + str(update.from_user.id) + "/"
+    download_location = download_path + "photo.jpg"
+    message = await update.reply_text(
+        text="`Processing...`",
+        quote=True
+    )
+    try:
+        await update.download(
+            file_name=download_location
+        )
+    except Exception as error:
+        await message.edit_text(
+            text=f"**Error :** `{error}`\nContact @i_am_albin_praveen."
+        )
+        return 
+    await message.edit_text(
+        text="`Converting to glitch...`"
+    )
+    try:
+        glitch_art = glitchart.jpeg(download_location)
+        await update.reply_photo(photo=glitch_art, quote=True)
+        os.remove(download_location)
+        os.remove(glitch_art)
+    except Exception as error:
+        await message.edit_text(
+            text=f"**Error :** `{error}`\nContact @i_am_albin_praveen."
+        )
+        return
+    await message.delete()
+
+
+Bot.run()
